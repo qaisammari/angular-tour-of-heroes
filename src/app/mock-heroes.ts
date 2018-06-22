a63c175f95b77c22f783c31afd05659ffd4a137c
Qais Ammari <qais.ammari@triphop.com>
2018-06-22 03:14:14 +0300

    added hero service to fetch the mock using observable, and added messages service

--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -1,14 +1,14 @@
 import { Hero } from './hero';
 
 export const HEROES: Hero[] = [
-  { id: 11, name: 'Mr. Nice' },
-  { id: 12, name: 'Narco' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
+  { id: 11, name: 'Mr. Nice', email: 'qemania@gmail.com'},
+  { id: 12, name: 'Narco', email: 'qemania@gmail.com' },
+  { id: 13, name: 'Bombasto' , email: 'qemania@gmail.com'},
+  { id: 14, name: 'Celeritas' , email: 'qemania@gmail.com'},
+  { id: 15, name: 'Magneta' , email: 'qemania@gmail.com'},
+  { id: 16, name: 'RubberMan' , email: 'qemania@gmail.com'},
+  { id: 17, name: 'Dynama' , email: 'qemania@gmail.com'},
+  { id: 18, name: 'Dr IQ' , email: 'qemania@gmail.com'},
+  { id: 19, name: 'Magma' , email: 'qemania@gmail.com'},
+  { id: 20, name: 'Tornado' , email: 'qemania@gmail.com'}
 ];
